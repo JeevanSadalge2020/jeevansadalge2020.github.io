e58a7a383eb5796160b33506b3d6b98f91d0c36a
JeevanSadalge2020 <jeevanhrx@gmail.com>
2021-06-22 19:38:57 +0530

    slider js finished for all

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,4 +1,4 @@
-$('.slick').slick({
+$('.slick-for-world-class-expert-doctors').slick({
     dots: true,
     infinite: false,
     speed: 300,
@@ -16,24 +16,64 @@ $('.slick').slick({
         }
     ]
 });
-// window.addEventListener("resize", removeClass);
-// window.addEventListener("load", removeClass);
+window.addEventListener("resize", removeClass);
+window.addEventListener("load", removeClass);
 
-// function removeClass() {
-//     let items = document.querySelectorAll(".world-class-expert-doctors__list__item");
-//     if (window.innerWidth < 1024) {
-//         items.forEach((item, index) => {
-//             if (index > 3) {
-//                 item.style.display = "none";
-//             }
-//         })
-//     }
-// }
+function removeClass() {
+    let items = document.querySelectorAll(".world-class-expert-doctors__list__item");
+    if (window.innerWidth < 1024) {
+        items.forEach((item, index) => {
+            if (index > 3) {
+                item.style.display = "none";
+            }
+        })
+    }
+}
 
-$(window).resize(function () {
-    $('.slick').not('.slick-initialized').slick('resize');
+$(window).on('resize', function () {
+    var win = $(this); //this = window
+    if (win.width() < 1080) {
+        removeClass();
+        let items = document.querySelectorAll(".world-class-expert-doctors__list__item");
+        items.forEach((item, index) => {
+            if (index > 3) {
+                item.style.display = "none";
+            }
+        })
+    }
 });
 
-$(window).on('orientationchange', function () {
-    $('.slick').not('.slick-initialized').slick('resize');
+
+// TO APPLY SLICK WHEN SWITCHING FROM UNSLICK
+$(window).resize(function () {
+    $('.slick-for-world-class-expert-doctors').not('.slick-initialized').slick('resize');
+});
+
+$(window).resize(function () {
+    $('.slick-for-medical-treatments').not('.slick-initialized').slick('resize');
+});
+
+// $(window).on('orientationchange', function () {
+//     $('.slick-for-world-class-expert-doctors').not('.slick-initialized').slick('resize');
+// });
+
+
+// FEATURED MEDICAL TREATMENTS
+$('.slick-for-medical-treatments').slick({
+    dots: true,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    responsive: [
+        {
+            breakpoint: 1023,
+            settings: "unslick"
+        },
+        {
+            breakpoint: 480,
+            settings: "unslick"
+        }
+    ]
 });
